0d62d4b0cd2e0b908a80426b16e394845cefd6de
LiisaKotilainen <liisa.kotilainen@edu.turkuamk.fi>
2025-01-16 18:37:17 +0200

    Kirjautumisfiksaus

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title and author, but not url or likes by default', () => {
